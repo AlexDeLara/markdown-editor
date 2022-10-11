00f0f58d5f57bb5b105e5739aa1ff21bb6aa3637
AlexDeLara <hfdelara@gmail.com>
2022-10-10 21:24:40 -0500

    Add sample Markdown value

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -4,6 +4,10 @@ import { updateTextInput } from "../features/textInput/textInput";
 import { useState } from "react";
 
 function MarkdownEditor() {
+  const sampleMarkdown = fetch("./data/sampleMarkdown").then((response) =>
+    response.text()
+  );
+
   const dispatch = useDispatch();
 
   const [visibility, setVisibility] = useState(true);
@@ -17,12 +21,17 @@ function MarkdownEditor() {
   }
 
   return (
-    <div className={`${visibility ? "h-1/5" : null} w-1/2 flex flex-col items-center`}>
+    <div
+      className={`${
+        visibility ? "h-1/5" : null
+      } w-1/2 flex flex-col items-center`}
+    >
       <WindowBar windowName="Editor" toggleVisibility={toggleVisibility} />
       {visibility && (
         <textarea
           onChange={handleChange}
           className="w-full h-full p-4 rounded-b focus:outline-0"
+          value={sampleMarkdown}
         ></textarea>
       )}
     </div>
